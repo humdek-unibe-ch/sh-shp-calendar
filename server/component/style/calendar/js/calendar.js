885130ab3fbeb5a6359541385e372b1261cf5a94
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-10-25 15:42:28 +0200

    adjust checkbox inputs

--- server/component/style/calendar/js/calendar.js
+++ server/component/style/calendar/js/calendar.js
@@ -24,6 +24,9 @@ function initCalendar() {
     $('#calendar-view').removeAttr('data-events');
     $('#calendar-view').removeAttr('data-data');
     console.log(calendar_data);
+    if(!calendar_data){
+        return;
+    }
     calendar = new FullCalendar.Calendar($('#calendar-view')[0], {
         initialView: 'dayGridMonth',
         themeSystem: 'bootstrap',
@@ -63,7 +66,8 @@ function initCalendar() {
                 text: calendar_data['label_calendar_add_event'],
                 click: function () {
                     $('#modal input[type="radio"]').prop('checked', false); //remove all set checked values
-                    $('#modal input[type!="hidden"]:not([type="radio"])').val('');
+                    $('#modal input[type="checkbox"]').prop('checked', false); //remove all set checked values
+                    $('#modal input[type!="hidden"]:not([type="radio"]):not([type="checkbox"])').val('');
                     $('#modal textarea[type!="hidden"]').val('');
                     $('input[name="selected_record_id"]').remove(); // remove the selected record if it is there
                     $("#modal").modal();
@@ -73,6 +77,7 @@ function initCalendar() {
         eventClick: function (info) {
             console.log(info.event.extendedProps);
             $('#modal input[type="radio"]').prop('checked', false); //remove all set checked values
+            $('#modal input[type="checkbox"]').prop('checked', false); //remove all set checked values
             if ($('input[name="selected_record_id"]').length === 0) {
                 // if the selected input record is not added, add it
                 var selectedRecord = $('<input>').attr({
@@ -86,12 +91,12 @@ function initCalendar() {
                 if (key.startsWith("_")) {
                     // set the value from the event
                     var fieldName = key.replace('_', '');
-                    var fieldNameSearch = 'input[name="' + fieldName + '[value]"]:not([type="radio"]), textarea[name="' + fieldName + '[value]"]';
+                    var fieldNameSearch = 'input[name="' + fieldName + '[value]"]:not([type="radio"]):not([type="checkbox"]), textarea[name="' + fieldName + '[value]"]';
                     $(fieldNameSearch).val(entryValues[key]);
                     // Search for radio input and textarea elements
-                    var fieldNameSearchRadio = 'input[name="' + fieldName + '[value]"][type="radio"]';
+                    var fieldNameSearchRadio = 'input[name="' + fieldName + '[value]"][type="radio"], input[name="' + fieldName + '[value]"][type="checkbox"]';
                     $(fieldNameSearchRadio).each(function () {
-                        if ($(this).is(':radio')) { // Check if it's a radio input
+                        if ($(this).is(':radio') || $(this).is(':checkbox')) { // Check if it's a radio input
                             if ($(this).val() == entryValues[key]) {
                                 $(this).prop('checked', true); // Set the "checked" property to true
                             }
