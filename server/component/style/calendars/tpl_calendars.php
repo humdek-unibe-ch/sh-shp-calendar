9ffc664b2a7439fb7b6cdf77f95cf7f5f9aaba12
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-11-01 12:41:16 +0100

    add css to the calendars holder

--- server/component/style/calendars/tpl_calendars.php
+++ server/component/style/calendars/tpl_calendars.php
@@ -3,6 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
-<div id="calendars-holder">
+<div id="calendars-holder" class="<?php echo $this->css; ?>">
     <?php $this->output_calendars(); ?>
 </div>
